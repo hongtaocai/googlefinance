c2dbb750d6a8bf2f7183dec318cab16a471351d2
hongtao <caiht.ht@gmail.com>
2015-03-03 10:17:32 +0800

    change api name

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'googlefinance',
   packages = ['googlefinance'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'Python module to get stock data from Google Finance API',
   author = 'Hongtao Cai',
   author_email = 'caiht.ht@gmail.com',
   url = 'https://github.com/hongtaocai/googlefinance', # use the URL to the github repo
-  download_url = 'https://github.com/hongtaocai/googlefinance/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/hongtaocai/googlefinance/tarball/0.2', # I'll explain this in a second
   keywords = ['googlefinance', 'stock', 'price' , 'finance', 'google'], # arbitrary keywords
   classifiers = [],
 )
